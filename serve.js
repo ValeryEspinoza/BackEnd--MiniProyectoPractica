9118a62f7a52a65744c9aca4688f7068a4f802c4
ValeryEspinoza <vespinoza@fwdcostarica.com>
2024-10-09 15:46:00 -0600

    Inicio del mini proyecto practica

---
+++ serve.js
@@ -0,0 +1,12 @@
+//Mini Practica
+/*[15:27, 10/9/2024] Vale🥋: Mini Proyecto:
+Creacion de una API RESTful básica
+-Manejo de peticiones
+-Crear, leer, actualizar y eliminar.
+CRUD
+[15:27, 10/9/2024] Vale🥋: Fin de la clase del  9 Oct 2024 Back End*/
+
+
+
+
+
